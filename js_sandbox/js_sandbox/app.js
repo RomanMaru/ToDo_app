76b9c619c76b761002d28cf01801c09774dac700
Roman Maru <romanmaru10@gmail.com>
2020-01-05 12:57:34 -0800

    Create HTML template, Define UI vars, AddTak listener on submit button

--- js_sandbox/js_sandbox/app.js
+++ js_sandbox/js_sandbox/app.js
@@ -1,40 +1,33 @@
-let val;
+// Define UI variables
+const form = document.querySelector('#task-form')
+const taskList = document.querySelector('.collection')
+const clearBtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
+const taskInput = document.querySelector('#task')
+//Load all event listeners
+loadEventListeners()
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+function loadEventListeners() {
+  form.addEventListener('submit', addTask)
+}
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+function addTask(event) {
+  if (taskInput.value === '') {
+    alert('Add a task')
+  }
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+  const li = document.createElement('li')
+  li.className = 'collection-item'
+  li.appendChild(document.createTextNode(taskInput.value))
+  
+  const link = document.createElement('a')
+  link.className = 'delete-item secondary-content'
+  link.innerHTML = '<i class="fa fa-remove"></i>'
 
-val = document.images;
+  li.appendChild(link)
+  taskList.appendChild(li)
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
-
-let scripts = document.scripts;
-
-let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
-});
-
-console.log(val);
+  taskInput.value = ''
+  
+  event.preventDefault()
+}
